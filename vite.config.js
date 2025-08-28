135ce6fb09b4c8cae01165b9de5beb0bb87fad6d
Pawan <pawank.dtu1@gmail.com>
2025-08-29 00:56:12 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {}, // prevent process undefined errors
+  },
   build: {
     lib: {
       entry: "src/main.jsx",
